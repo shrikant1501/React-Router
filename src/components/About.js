350aba9e1f0aa35c49ccafc7f2b05a7177d069fe
SHRIKANT THAKRE <thakesrikant@gmail.com>
2024-05-17 16:42:04 +0530

    Class component added

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
+import UserClass from "./User";
 const About = () => {
   return (
     <div>
       <h1>This is about page</h1>
       <h2>We provide quality food!!!</h2>
+      <UserClass />
     </div>
   );
 };
---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+class UserClass extends react.Component {
+  render() {
+    return (
+      <div>
+        <h2>Shrikant</h2>
+        <h3>Akola</h3>
+        <h4>shrikant15@gmail.com</h4>
+      </div>
+    );
+  }
+}
+export default UserClass;
